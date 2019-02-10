8084e59cb40ee8768cd965e0c31fd83d0b20072e
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-02-11 02:45:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var wechat = require('wechat');
 app.use(express.query());
 app.use('/wechat', wechat('zengjinzhe').text(function (message, req, res, next) {
     res.reply(chemicaltoolsbot.reply(message))
-})
+}))
