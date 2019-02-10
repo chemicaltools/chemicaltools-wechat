8275fe84f3f3fc69a2282aad48e28e568d7c9bc3
Jinzhe Zeng <njzjz@qq.com>
2019-02-11 02:53:18 +0800

    fix: update token

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var chemicaltoolsbot = require('chemicaltoolsbot')
 var wechat = require('wechat');
+var config = {
+    token: 'zengjinzhe',
+    appid: 'wx0cd8b5047421a3aa',
+    encodingAESKey: 'HQGUNPgz1fogojffKg0KDl6eW5UMEsdlhwGo0CdFWQj',
+    checkSignature: false
+  };
 app.use(express.query());
-app.use('/wechat', wechat('zengjinzhe').text(function (message, req, res, next) {
+app.use('/wechat', wechat(config).text(function (message, req, res, next) {
     res.reply(chemicaltoolsbot.reply(message))
 }))
